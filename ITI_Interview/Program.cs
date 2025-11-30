059a0539b9b037faf2a4234620cba9438e809fb7
Bilal <blalhamd48@gmail.com>
2025-11-30 23:05:50 +0200

    change target framework from .Net 9.0 to .Net 10.0 and added the first program in the main method

--- ITI_Interview/Program.cs
+++ ITI_Interview/Program.cs
@@ -3,20 +3,13 @@ internal class Program
 {
     private static void Main(string[] args)
     {
-   
-        
+        int FirstNumber = 10;
+        int SecondNumber = 20;
+        int Sum = FirstNumber + SecondNumber;
+        Console.WriteLine("The sum of {0} and {1} is {2}", FirstNumber, SecondNumber, Sum);
+
 
 
         Console.ReadKey();
-    }
-
-    public static void Set(int x)
-    {
-
-    }
-
-    public static void Set(int x, int y)
-    {
-
     }
 }
