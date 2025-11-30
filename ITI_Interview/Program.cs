1e59009a97220caf8de8ce659d815e398101622e
Bilal <blalhamd48@gmail.com>
2025-12-01 00:57:29 +0200

    using toString() method to print employee

--- ITI_Interview/Program.cs
+++ ITI_Interview/Program.cs
@@ -9,7 +9,7 @@ internal class Program
         double FirstNumber = 10;
         double SecondNumber = 20;
         double Sum = FirstNumber + SecondNumber;
-        Console.WriteLine("The sum of {0} and {1} is {2}", FirstNumber, SecondNumber, Sum);
+        Console.WriteLine("The sum of {0} and {1} is {2}\n", FirstNumber, SecondNumber, Sum);
 
         // create employee object
         var employee = new Empolyee
@@ -20,20 +20,16 @@ internal class Program
             DepartmentId = 101
         };
 
-        Console.WriteLine("Employee Details: Id={0}, Name={1}, Salary={2} egy, DepartmentId={3}", employee.Id, employee.Name, employee.Salary,employee.DepartmentId);
+        Console.WriteLine(employee);
 
         // create department object
         var department = new Department
         {
             Id = 101,
-<<<<<<< HEAD
-            Name = "MM"  
-=======
             Name = "CS"  
->>>>>>> feature
         };
 
-        Console.WriteLine("Department Details: Id={0}, Name={1}", department.Id, department.Name);
+        Console.WriteLine("\nDepartment Details: Id={0}, Name={1}\n", department.Id, department.Name);
 
 
         Console.ReadKey();
