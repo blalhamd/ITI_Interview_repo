7e03dfd6fd620d2b5d1e72b33290bd27e117143a
Bilal Sayed Mohamed <141357295+blalhamd@users.noreply.github.com>
2025-12-01 01:27:11 +0200

    change empty to  = null!;

--- ITI_Interview/Empolyee.cs
+++ ITI_Interview/Empolyee.cs
@@ -3,7 +3,7 @@
     public class Empolyee
     {
         public int Id { get; set; }
-        public string Name { get; set; } = string.Empty;
+        public string Name { get; set; } = null!;
         public double Salary { get; set; }
         public int DepartmentId { get; set; }
         public Department Department { get; set; } = null!;
