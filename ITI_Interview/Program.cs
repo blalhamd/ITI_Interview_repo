d9038314701a30b9c10bd9d3cb9e01022999f69a
Bilal <blalhamd48@gmail.com>
2025-12-01 21:19:23 +0200

    add ToString() during printing

--- ITI_Interview/Program.cs
+++ ITI_Interview/Program.cs
@@ -7,11 +7,11 @@ internal class Program
     {
         // create employee object
         var employee = new Empolyee(1, "John Doe", 20000, 101);
-        Console.WriteLine(employee);
+        Console.WriteLine(employee.ToString());
 
         // create department object
         var department = new Department(101, "CS");
-        Console.WriteLine(department);
+        Console.WriteLine(department.ToString());
 
 
         Console.ReadKey();
