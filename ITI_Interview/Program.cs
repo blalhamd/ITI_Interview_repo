1403655793ab75727b03317a6929931fa9457460
Bilal <blalhamd48@gmail.com>
2025-12-03 23:56:20 +0200

    change names to arabic names

--- ITI_Interview/Program.cs
+++ ITI_Interview/Program.cs
@@ -3,7 +3,7 @@ public partial class Program
     private static void Main(string[] args)
     {
         // create employee object
-        var employee = new Empolyee(1, "John Doe", 20000, 101);
+        var employee = new Empolyee(1, "Abdo Badawy", 20000, 101);
         Console.WriteLine(employee);
 
         // create department object
@@ -11,7 +11,7 @@ public partial class Program
         Console.WriteLine(department);
 
         // create manager object
-        var manager = new Manager(1, "Jane Smith");
+        var manager = new Manager(1, "Mostafa Mohamed");
         Console.WriteLine(manager);
 
         Console.ReadKey();
