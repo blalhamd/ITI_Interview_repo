f05e163110c9a7212eb9f2c9fe1b0d5e2c0b3808
Bilal <blalhamd48@gmail.com>
2025-12-10 22:13:56 +0200

    add dbcontext class with content it

---
+++ ITI_Interview/Data/ITIDbContext.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace ITI_Interview.Data
+{
+    public class ITIDbContext : DbContext
+    {
+        public DbSet<Empolyee> Employees { get; set; }
+        public DbSet<Department> Departments { get; set; }
+        public DbSet<Manager> Managers { get; set; }
+        public ITIDbContext(DbContextOptions<ITIDbContext> options) : base(options)
+        {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+        }
+    }
+}
