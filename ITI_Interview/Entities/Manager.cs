ebc1918dce29b83ca64fc3867e47cdaab52c0530
Bilal <blalhamd48@gmail.com>
2025-12-10 22:11:23 +0200

    add application dbcontext class

--- ITI_Interview/Entities/Manager.cs
+++ ITI_Interview/Entities/Manager.cs
@@ -1,39 +1,36 @@
-public partial class Program
+public class Manager
 {
-    public class Manager
+    public int Id { get; private set; }
+    public string FullName { get; private set; } = null!;
+
+    public Manager(int id, string fullName)
     {
-        public int Id { get; private set; }
-        public string FullName { get; private set; } = null!;
+        SetId(id);
+        SetName(fullName);
+    }
 
-        public Manager(int id, string fullName)
-        {
-            SetId(id);
-            SetName(fullName);
-        }
+    private void SetId(int id)
+    {
+        if (id <= 0)
+            throw new ArgumentException("Id must be greater than zero.");
+        Id = id;
+    }
 
-        private void SetId(int id)
-        {
-            if(id <= 0)
-                throw new ArgumentException("Id must be greater than zero.");
-            Id = id;
-        }
-        
-        public void SetName(string name)
-        {
-            if(string.IsNullOrWhiteSpace(name))
-                throw new ArgumentException("Name cannot be null or empty.");
-            FullName = name;
-        }
+    public void SetName(string name)
+    {
+        if (string.IsNullOrWhiteSpace(name))
+            throw new ArgumentException("Name cannot be null or empty.");
+        FullName = name;
+    }
 
-        // يحسب الضريبة 10%
-        public double CalculateTax(double salary)
-        {
-            return salary * 0.10;
-        }
+    // يحسب الضريبة 10%
+    public double CalculateTax(double salary)
+    {
+        return salary * 0.10;
+    }
 
-        public override string ToString()
-        {
-            return $"Manager [Id={Id}, Name={FullName}]";
-        }
+    public override string ToString()
+    {
+        return $"Manager [Id={Id}, Name={FullName}]";
     }
 }
